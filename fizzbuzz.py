5e878ff25d3fdaadd5e49e945766467f23f49991
Mike Foster <i.am.mike.foster@gmail.com>
2022-09-17 15:44:05 +0200

    functional fizzbuzzer with typehinting in 25 awesome lines

---
+++ fizzbuzz.py
@@ -0,0 +1,25 @@
+from typing import Any, Callable
+
+
+def modulo(n: int) -> Callable[[int], bool]:
+    def _modulo(x: int) -> bool:
+        return x % n == 0
+    return _modulo
+
+
+def joiner(rules: dict) -> Callable[[Any], Any]:
+    def _joiner(val):
+        result = ''.join(rtn for f, rtn in rules.items() if f(val))
+        if result:
+            return result
+        else:
+            return val
+    return _joiner
+
+
+rules = {
+    modulo(3): 'fizz',
+    modulo(5): 'buzz'
+}
+
+fizzbuzzer = joiner(rules)
