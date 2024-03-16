8fa64a6a934bca9a88eb865433d43de00ee0728a
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-16 08:26:56 +0000

    use googletest expect_that!

--- tests/test_f64.rs
+++ tests/test_f64.rs
@@ -1,8 +1,10 @@
 use fizzbuzz::f64;
 use fizzbuzz::FizzBuzz;
+use googletest::*;
+use googletest::prelude::*;
 
-#[test]
-fn test_f64() {
+#[googletest::test]
+fn googletest_f64() {
     let allnums: Vec<f64> = (1i16..=300).step_by(1).map(|i| f64::from(i)).collect();
     let fifteens: Vec<f64> = (0i16..=300).step_by(15).map(|i| f64::from(i)).collect();
     let fives: Vec<f64> = (0i16..=300).step_by(5).map(|i| f64::from(i)).collect();
@@ -11,13 +13,13 @@ fn test_f64() {
     for num in allnums {
         let result = num.fizzbuzz();
         if fifteens.contains(&num) {
-            assert_eq!(result, "fizzbuzz")
+            expect_that!(result, eq("fizzbuzz"))
         } else if fives.contains(&num) {
-            assert_eq!(result, "buzz")
+            expect_that!(result, eq("buzz"))
         } else if threes.contains(&num) {
-            assert_eq!(result, "fizz")
+            expect_that!(result, eq("fizz"))
         } else {
-            assert_eq!(&result, &num.to_string())
+            expect_that!(&result, eq(&num.to_string()))
         }
     }
 }
