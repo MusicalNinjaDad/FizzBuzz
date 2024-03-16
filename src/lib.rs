ad0bd8bcca608e1af2d107cd663d7731050bcb48
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-16 09:25:12 +0000

    and for u8

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@ macro_rules! impl_fizzbuzz {
     };
 }
 
-impl_fizzbuzz!(f32, f64, i16, i32, i64, i128, u16, u32, u64, u128, usize);
+impl_fizzbuzz!(f32, f64, i16, i32, i64, i128, u8, u16, u32, u64, u128, usize);
 
 #[cfg(test)]
 mod dumbtests {
