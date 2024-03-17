dca007104b957ca558740949e4e0cbf7bfbce773
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-17 20:31:23 +0000

    Add documentation

--- src/fizzbuzz/__init__.py
+++ src/fizzbuzz/__init__.py
@@ -1,1 +1,21 @@
+"""
+A python implementation of fizzbuzz using closures and a dictionary of rules.
+
+Trying to find a pythonic approach was surprisingly difficult as most of what is considered to be pythonic doesn't
+make sense in fizzbuzz. In the end I went for an approach using closures, a dictionary lookup and a generator
+expression.
+
+## Examples:
+
+```
+>>> fizzbuzzer(1)
+'1'
+```
+
+```
+>>> fizzbuzzer(3)
+'fizz'
+```
+
+"""
 from .fizzbuzzer import fizzbuzzer
--- src/fizzbuzz/fizzbuzzer.py
+++ src/fizzbuzz/fizzbuzzer.py
@@ -1,22 +1,35 @@
-from collections import OrderedDict  # noqa: D100
+"""Implements fizzbuzzer() function."""
+from collections import OrderedDict
 from typing import Any, Callable
 
 
-def modulo(n: int) -> Callable[[int], bool]:  # noqa: D103
+def modulo(n: int) -> Callable[[int], bool]:
+    """
+    Returns a function to test whether a value is directly divisible by n.
+    
+    Example:
+    ```
+    >>> even = modulo(2)
+    >>> even(4)
+    True
+    >>> even(3)
+    False
+    ```
+    """
     def _modulo(x: int) -> bool:
         return x % n == 0
 
     return _modulo
 
 
-def joiner(rules: dict) -> Callable[[Any], Any]:  # noqa: D103
-    def _joiner(val: int) -> str:
+def _joiner(rules: dict) -> Callable[[Any], Any]:
+    def __joiner(val: int) -> str:
         result = "".join(rtn for f, rtn in rules.items() if f(val))
         if result:
             return result
         return str(val)
 
-    return _joiner
+    return __joiner
 
 
 rules = OrderedDict(
@@ -26,4 +39,4 @@ rules = OrderedDict(
     },
 )
 
-fizzbuzzer = joiner(rules)
+fizzbuzzer = _joiner(rules)
