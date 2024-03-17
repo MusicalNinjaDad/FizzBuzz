263f8409c04de4c718de6139d95d4659eb902cc3
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-17 20:13:52 +0000

    lint

--- src/fizzbuzz/fizzbuzzer.py
+++ src/fizzbuzz/fizzbuzzer.py
@@ -5,21 +5,25 @@ from typing import Any, Callable
 def modulo(n: int) -> Callable[[int], bool]:  # noqa: D103
     def _modulo(x: int) -> bool:
         return x % n == 0
+
     return _modulo
 
 
 def joiner(rules: dict) -> Callable[[Any], Any]:  # noqa: D103
-    def _joiner(val):  # noqa: ANN001
+    def _joiner(val: int) -> str:
         result = "".join(rtn for f, rtn in rules.items() if f(val))
         if result:
             return result
         return str(val)
+
     return _joiner
 
 
-rules = OrderedDict({
-    modulo(3): "fizz",
-    modulo(5): "buzz"  # noqa: COM812
-})
+rules = OrderedDict(
+    {
+        modulo(3): "fizz",
+        modulo(5): "buzz",
+    },
+)
 
 fizzbuzzer = joiner(rules)
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -13,22 +13,17 @@ def test_lazy():
 
 def test_rules():
     results = [fizzbuzzer(i) for i in range(1, 101)]
-    Every3rdHasFizz = all("fizz" in r for r in results[2::3])  # noqa: N806
-    assert Every3rdHasFizz
-    Every5thHasBuzz = all("buzz" in r for r in results[4::5])  # noqa: N806
-    assert Every5thHasBuzz
-    Every15thIsFizzBuzz = all(r == "fizzbuzz" for r in results[14::15])  # noqa: N806
-    assert Every15thIsFizzBuzz
-    EveryFizzIsMod3 = all((i+1) % 3 == 0 for i,  # noqa: N806
-                          r in enumerate(results) if r == "fizz")
-    assert EveryFizzIsMod3
-    EveryBuzzIsMod5 = all((i+1) % 5 == 0 for i,  # noqa: N806
-                          r in enumerate(results) if r == "buzz")
-    assert EveryBuzzIsMod5
-    EveryFizzBuzzIsMod15 = all(  # noqa: N806
-        (i+1) % 15 == 0 for i, r in enumerate(results) if r == "fizzbuzz")
-    assert EveryFizzBuzzIsMod15
-    AllNumbersCorrect = all(  # noqa: N806
-        r == str(i+1) for i, r in enumerate(results) if r not in ("fizz", "buzz", "fizzbuzz")
-    )
-    assert AllNumbersCorrect
+    every_3rd_has_fizz = all("fizz" in r for r in results[2::3])
+    assert every_3rd_has_fizz
+    every_5th_has_buzz = all("buzz" in r for r in results[4::5])
+    assert every_5th_has_buzz
+    every_15th_is_fizzbuzz = all(r == "fizzbuzz" for r in results[14::15])
+    assert every_15th_is_fizzbuzz
+    every_fizz_is_mod3 = all((i + 1) % 3 == 0 for i, r in enumerate(results) if r == "fizz")
+    assert every_fizz_is_mod3
+    every_buzz_is_mod5 = all((i + 1) % 5 == 0 for i, r in enumerate(results) if r == "buzz")
+    assert every_buzz_is_mod5
+    every_fizzbuzz_is_mod15 = all((i + 1) % 15 == 0 for i, r in enumerate(results) if r == "fizzbuzz")
+    assert every_fizzbuzz_is_mod15
+    all_numbers_correct = all(r == str(i + 1) for i, r in enumerate(results) if r not in ("fizz", "buzz", "fizzbuzz"))
+    assert all_numbers_correct
