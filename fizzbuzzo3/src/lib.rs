fc01d7ce715625f08de1f50cbd5bf1cda4cae75c
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-22 06:10:36 +0000

    update to pyo3 0.21.0-beta.0 to get new Bound API

--- fizzbuzzo3/src/lib.rs
+++ fizzbuzzo3/src/lib.rs
@@ -9,7 +9,7 @@ fn py_fizzbuzz(num: i32) -> String {
 
 #[pymodule]
 #[pyo3(name = "fizzbuzzo3")]
-fn py_fizzbuzzo3(_py: Python, module: &PyModule) -> PyResult<()> {
+fn py_fizzbuzzo3(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(py_fizzbuzz, module)?)?;
     Ok(())
 }
@@ -26,9 +26,9 @@ mod tests {
         pyo3::append_to_inittab!(py_fizzbuzzo3);
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
-            let fizzbuzzo3 = py.import("fizzbuzzo3").expect("Failed to import fizzbuzzo3");
+            let fizzbuzzo3 = py.import_bound("fizzbuzzo3").expect("Failed to import fizzbuzzo3");
             let fizzbuzz = fizzbuzzo3.getattr("fizzbuzz").expect("Failed to get fizzbuzz function");
-            let result = fizzbuzz.call((1i32,), None).expect("Failed to call fizzbuzz");
+            let result = fizzbuzz.call1((1i32,)).expect("Failed to call fizzbuzz");
             let expected_result = "1";
             assert_eq!(result.extract::<String>().unwrap(), expected_result);
         });
