560545a5fc31fbec8834c30108f81f00dfffaa44
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-22 18:11:40 +0000

    RED: enable fizzbuzzo3 on float

--- fizzbuzzo3/src/lib.rs
+++ fizzbuzzo3/src/lib.rs
@@ -60,4 +60,25 @@ mod tests {
             assert!(result.unwrap());
         });
     }
+
+    #[test]
+    fn test_fizzbuzz_float() {
+        pyo3::append_to_inittab!(py_fizzbuzzo3);
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| {
+            let fizzbuzzo3 = py
+                .import_bound("fizzbuzzo3")
+                .expect("Failed to import fizzbuzzo3");
+            let fizzbuzz = fizzbuzzo3
+                .getattr("fizzbuzz")
+                .expect("Failed to get fizzbuzz function");
+            let result: PyResult<String> = match fizzbuzz.call1((1f32,)) {
+                Ok(r) => r.extract(),
+                Err(e) => Err(e),
+            };
+            let result = result.unwrap();
+            let expected_result = "1.0";
+            assert_eq!(result, expected_result);
+        });
+    }
 }
