e654335c8792ebc39b984731fa5cfd5409e5419e
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-25 05:37:33 +0000

    also import the function

--- pyo3testing/src/lib.rs
+++ pyo3testing/src/lib.rs
@@ -2,28 +2,34 @@ use quote::quote;
 use proc_macro2::{TokenStream as TokenStream2, Span};
 use syn::Ident;
 
-fn importmodule(module: Pyo3Import, input: TokenStream2) -> TokenStream2 {
+fn import_pyo3_from(import: Pyo3Import, input: TokenStream2) -> TokenStream2 {
     
-    let moduleident = module.identifier;
-    let pymoduleident = Ident::new(&module.name, Span::mixed_site());
-    let modulename = module.name;
+    let moduleident = import.moduleidentifier;
+    let pymoduleident = Ident::new(&import.modulename, Span::mixed_site());
+    let modulename = import.modulename;
     let modulerror = "Failed to import ".to_string() + &modulename;
+    let functionname = import.functionname;
+    let functionerror = "Failed to get ".to_string() + &functionname + &" function".to_string();
 
     quote!(
         pyo3::append_to_inittab!(#moduleident);
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
             let #pymoduleident = py
-            .import_bound(#modulename)
-            .expect(#modulerror);
+                .import_bound(#modulename)
+                .expect(#modulerror);
+            let fizzbuzz = fizzbuzzo3
+                .getattr(#functionname)
+                .expect(#functionerror);
             #input
         });
     )
 }
 
 struct Pyo3Import {
-    identifier: Ident,
-    name: String
+    moduleidentifier: Ident,
+    modulename: String,
+    functionname: String,
 }
 
 #[cfg(test)]
@@ -39,8 +45,9 @@ mod tests {
         let py_fizzbuzzo3 = Ident::new("py_fizzbuzzo3", Span::call_site());
 
         let module = Pyo3Import {
-            identifier: py_fizzbuzzo3,
-            name: "fizzbuzzo3".to_string()
+            moduleidentifier: py_fizzbuzzo3,
+            modulename: "fizzbuzzo3".to_string(),
+            functionname: "fizzbuzz".to_string(),
         };
 
         let expected = quote!{
@@ -50,11 +57,14 @@ mod tests {
                 let fizzbuzzo3 = py
                 .import_bound("fizzbuzzo3")
                 .expect("Failed to import fizzbuzzo3");
+                let fizzbuzz = fizzbuzzo3
+                .getattr("fizzbuzz")
+                .expect("Failed to get fizzbuzz function");
                 assert!(true);
             });
         };
 
-        let output = importmodule(module, input);
+        let output = import_pyo3_from(module, input);
 
         assert_eq!(output.to_string(), expected.to_string());
 
