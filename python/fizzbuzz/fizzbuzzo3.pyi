2583076a667f2ccda0769a32222270e03bd3e831
Mike Foster <i.am.mike.foster@gmail.com>
2024-05-15 17:23:06 +0000

    add a meaningful docstring and TEST docstrings in `pyi` files

--- python/fizzbuzz/fizzbuzzo3.pyi
+++ python/fizzbuzz/fizzbuzzo3.pyi
@@ -1,6 +1,32 @@
 #ruff: noqa: PYI021
 """Module level Docstring for fizzbuzzo3."""
 
+def fizzbuzz(n: int | list[int]) -> str:
+    """
+    Returns the correct fizzbuzz answer for any number or list of numbers.
 
-def fizzbuzz(n: float | list[float]) -> str:
-    """Rust implementation of fizzbuzz."""
+    This is an optimised algorithm compiled in rust. Large lists will utilise multiple CPU cores for processing.
+
+    Arguments:
+        n: the number(s) to fizzbuzz
+
+    Returns:
+        A `str` with the correct fizzbuzz answer(s).
+        In the case of a list of inputs the answers will be separated by `, ` 
+
+    Examples:
+        a single `int`:
+        ```
+        >>> from fizzbuzz.fizzbuzzo3 import fizzbuzz
+        >>> fizzbuzz(1)
+        '1'
+        >>> fizzbuzz(3)
+        'fizz'
+        ```
+        a `list`:
+        ```
+        from fizzbuzz.fizzbuzzo3 import fizzbuzz
+        >>> fizzbuzz([1,3])
+        '1, fizz'
+        ```
+    """
