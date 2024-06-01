25e9402d4804d34ffa2daabada33b8b92e334c53
Mike Foster <i.am.mike.foster@gmail.com>
2024-06-01 10:36:58 +0000

    reset to: use size_hint to decide whether to run in parallel or not be43710d95ba2804324c2211bf10713b849d6f7b

--- rust/fizzbuzz/tests/test_vec.rs
+++ rust/fizzbuzz/tests/test_vec.rs
@@ -1,4 +1,3 @@
-#![cfg(x)]
 use fizzbuzz::MultiFizzBuzz;
 
 mod vectors {
@@ -8,24 +7,6 @@ mod vectors {
     #[test]
     fn test_small_vec() {
         let input = vec![1, 2, 3, 4, 5];
-        let answer: Vec<String> = input.fizzbuzz().into();
-        let expected = vec![
-            "1".to_string(),
-            "2".to_string(),
-            "fizz".to_string(),
-            "4".to_string(),
-            "buzz".to_string(),
-        ];
-        assert_eq!(answer, expected)
-    }
-}
-
-mod ranges {
-    use super::*;
-
-    #[test]
-    fn test_1_to_5() {
-        let input = 1..=5;
         let answer: Vec<String> = input.fizzbuzz().into();
         let expected = vec![
             "1".to_string(),
