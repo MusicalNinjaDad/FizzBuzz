90b9dc42b1d9cf3f1ba75e531074a9957068fc54
Mike Foster <i.am.mike.foster@gmail.com>
2024-06-03 12:21:57 +0000

    Exclude impossible to test case from coverage

--- tests/test_fizzbuzzo3.py
+++ tests/test_fizzbuzzo3.py
@@ -42,6 +42,7 @@ def test_1_to_100():
 def test_slice():
      assert fizzbuzz(slice(1,16,1)) == "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz"
 
+# This case is REALLY IMPORTANT as it cannot be tested via rust unit tests...
 def test_slice_no_step():
      assert fizzbuzz(slice(1,16)) == "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz"
 
